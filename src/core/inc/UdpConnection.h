eb8203badf7936436565e089f188b25927001236
sz-piotr <szlachciak.piotr@gmail.com>
2017-05-22 15:12:13 +0200

    implement Connection interface in UdpConnection

--- src/core/inc/UdpConnection.h
+++ src/core/inc/UdpConnection.h
@@ -7,11 +7,19 @@
 #include <cstring>
 #include <netinet/in.h>
 
+struct sockaddr_4or6 {
+    bool ipv4;
+    struct sockaddr_in addr4;
+    struct sockaddr_in6 addr6;
+};
+
 class UdpConnection {
 private:
-    int socket_fd;
     bool open = false;
 public:
+    /* TEMPORARILY PUBLIC! Remove all usages */
+    int socket_fd;
+
     static constexpr const char* LOCALHOST = "127.0.0.1";
 
     ~UdpConnection();
@@ -21,16 +29,19 @@ public:
 
     void bind_port(uint16_t port);
 
-    void send(uint8_t* buffer, size_t buffer_size, const std::string& addr, in_port_t port);
+    /* DEPRECATED */
+    void send_data(uint8_t* data, uint16_t size, in_port_t port, std::string addr);
+
+    sockaddr_4or6 getAddress(std::string& addr, in_port_t port);
+    void send_msg(uint8_t* buffer, size_t len, sockaddr_4or6& address);
+
     void receive(uint8_t* buffer, size_t buffer_size, size_t& data_length);
 
     void raiseError(const char *message, const char *error = strerror(errno)) {
         std::stringstream ss;
         ss << "UdpConnection: "
            << message
-           << " ("
-           << error
-           << ")";
+           << " (" << error << ")";
         throw std::runtime_error(ss.str());
     }
 };
--- src/core/src/UdpConnection.cpp
+++ src/core/src/UdpConnection.cpp
@@ -45,33 +45,48 @@ UdpConnection::~UdpConnection() {
     close_socket();
 }
 
-void UdpConnection::send(uint8_t* buffer, size_t buffer_size, const std::string& addr, in_port_t port) {
-    struct sockaddr_in sa;
-    sa.sin_family = AF_INET;
-    sa.sin_port = htons(port);
+void UdpConnection::send_data(uint8_t* data, uint16_t size, in_port_t port, std::string addr) {
+    logWarn() << "UdpConnection: Method UdpConnection::send_data() is deprecated. Use UdpConnection::send_msg()";
 
-    struct sockaddr_in6 sa6;
-    sa6.sin6_family = AF_INET6;
-    sa6.sin6_port = htons(port);
+    struct sockaddr_in srv_addr;
+    srv_addr.sin_family = AF_INET;
+    inet_aton(addr.c_str(), &srv_addr.sin_addr);
+    srv_addr.sin_port = htons(port);
+    if (sendto(_socket, data, size, 0, (struct sockaddr*)&srv_addr, sizeof(srv_addr)) == -1) {
+        logError() << "Socket creation failed (" << strerror(errno) << ")";
+    }
+}
 
-    bool ipv4 = true;
+sockaddr_4or6 UdpConnection::getAddress(std::string& addr, in_port_t port) {
+    sockaddr_4or6 address;
 
-    auto p_res = inet_pton(AF_INET, addr.c_str(), &sa.sin_addr);
+    address.ipv4 = true;
+    address.addr4.sin_family = AF_INET;
+    address.addr4.sin_port = port;
+    address.addr6.sin6_family = AF_INET6;
+    address.addr6.sin6_port = port;
+
+    auto p_res = inet_pton(AF_INET, addr.c_str(), &address.addr4.sin_addr);
     if(p_res != 1) {
-        ipv4 = false;
-        p_res = inet_pton(AF_INET6, addr.c_str(), &sa6.sin6_addr);
+        address.ipv4 = false;
+        p_res = inet_pton(AF_INET6, addr.c_str(), &address.addr6.sin6_addr);
         if(p_res != 1) {
             raiseError("inet_pton() failed");
         }
     }
 
+    return address;
+}
+
+
+void UdpConnection::send_msg(uint8_t *buffer, size_t len, sockaddr_4or6 &address) {
     ssize_t s_res;
-    if(ipv4) {
+    if(address.ipv4) {
         logDebug() << "UdpConnection: Sending via ipv4";
-        s_res = sendto(socket_fd, buffer, buffer_size, 0, (sockaddr*) &sa, sizeof(sa));
+        s_res = sendto(socket_fd, buffer, len, 0, (sockaddr*) &address.addr4, sizeof(address.addr4));
     } else {
         logDebug() << "UdpConnection: Sending via ipv6";
-        s_res = sendto(socket_fd, buffer, buffer_size, 0, (sockaddr*) &sa6, sizeof(sa6));
+        s_res = sendto(socket_fd, buffer, len, 0, (sockaddr*) &address.addr6, sizeof(address.addr6));
     }
 
     if(s_res < 0) {
